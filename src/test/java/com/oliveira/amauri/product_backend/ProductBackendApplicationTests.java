b787312e4515a10ac8049f0d369cb015e23e562d
Amauri Oliveira <amauriibate32@hotmail.com>
2024-06-22 23:21:31 -0300

    Commit inicial

---
+++ src/test/java/com/oliveira/amauri/product_backend/ProductBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oliveira.amauri.product_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
