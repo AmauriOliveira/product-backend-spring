69a89c9b01f5b0e86c3a13f1ef95abf10f5d6225
Amauri Oliveira <amauriibate32@hotmail.com>
2024-06-24 21:51:07 -0300

    Cria Product endpoint com exemplos simples

--- backend/src/main/java/com/oliveira/amauri/product_backend/controllers/HelloController.java
+++ backend/src/main/java/com/oliveira/amauri/product_backend/controllers/HelloController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
   /**
-    * @return Hello World
-  */
+   * @return Hello World
+   */
   @GetMapping("")
   public String getHello() {
     return "Hello World";
