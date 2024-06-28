4a53d8af51456e5c793dc0f7be4c9fc0e55f65d2
Amauri Oliveira <amauriibate32@hotmail.com>
2024-06-27 23:10:11 -0300

    Ajuste na criação do array de produtos em memoria

--- backend/src/main/java/com/oliveira/amauri/product_backend/controllers/ProductController.java
+++ backend/src/main/java/com/oliveira/amauri/product_backend/controllers/ProductController.java
@@ -1,6 +1,6 @@
 package com.oliveira.amauri.product_backend.controllers;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
@@ -12,23 +12,16 @@ import org.springframework.web.server.ResponseStatusException;
 
 import com.oliveira.amauri.product_backend.models.Product;
 
-import jakarta.annotation.PostConstruct;
-
 @RestController
 public class ProductController {
 
-  private List<Product> products = new ArrayList<>();
+  private List<Product> products = Arrays.asList(
+    new Product(1, "Doce", 0.5),
+    new Product(2, "Pipoca", 0.7),
+    new Product(3, "Hot Dog", 6.5)
+  );
 
-  @PostConstruct // chama após a construção
-  public void init() {
-    Product product1 = new Product(1, "Doce", 0.5);
-    Product product2 = new Product(2, "Pipoca", 0.7);
-    Product product3 = new Product(3, "Hot Dog", 6.5);
-
-    this.products.add(product1);
-    this.products.add(product2);
-    this.products.add(product3);
-  }
+  //@PostConstruct // chama após a construção, pode se decorar uma função
 
   @GetMapping("products/{id}")
   public ResponseEntity<Product> getProduct(@PathVariable int id) {
